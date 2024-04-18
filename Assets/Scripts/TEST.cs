647f49a87adba25acf9e70ca5697f19c1aa1bfc4
Rinaldo da Silva Bento Junior <rinaldobrs@gmail.com>
2024-04-18 12:49:04 -0300

    feature: Screen reader disclaimer logic implemented

---
+++ Assets/Scripts/TEST.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TEST : MonoBehaviour
+{
+    void Update()
+    {
+        if (PlayerPrefs.GetInt("hasSeenDisclaimer",0) == 0 && UAP_AccessibilityManager.IsEnabled()) {
+            SceneManager.LoadScene("Disclaimer");
+            PlayerPrefs.SetInt("hasSeenDisclaimer",1);
+        }
+        else if (PlayerPrefs.GetInt("hasSeenDisclaimer",0) != 0) {
+            Destroy(gameObject.GetComponent<TEST>());
+        }
+    }
+}
